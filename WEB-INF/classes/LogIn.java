ae594bd4c4e8ee9070321616cc6c36b4e37ef04b
Remicchi <80085164+Remicchi@users.noreply.github.com>
2021-03-05 19:52:30 +0800

    Add files via upload

---
+++ WEB-INF/classes/LogIn.java
@@ -0,0 +1,62 @@
+// To save as "<TOMCAT_HOME>\webapps\hello\WEB-INF\classes\QueryServlet.java".
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/login")   // Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
+public class LogIn extends HttpServlet {
+
+   // The doGet() runs once per HTTP GET request to this servlet.
+   @Override
+   public void doPost(HttpServletRequest request, HttpServletResponse response)
+               throws ServletException, IOException {
+      // Set the MIME type for the response message
+      response.setContentType("text/html");
+      // Get a output writer to write the response message into the network socket
+      PrintWriter out = response.getWriter();
+
+      // Print an HTML page as the output of the query
+      out.println("<!DOCTYPE html>");
+      out.println("<html>");
+      out.println("<head><title>Query Response</title></head>");
+      out.println("<body>");
+
+      try (
+         // Step 1: Allocate a database 'Connection' object
+         Connection conn = DriverManager.getConnection(
+               "jdbc:mysql://localhost:3306/eshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+               "myuser", "xxxx");   // For MySQL
+               // The format is: "jdbc:mysql://hostname:port/databaseName", "username", "password"
+
+         // Step 2: Allocate a 'Statement' object in the Connection
+         Statement stmt = conn.createStatement();
+      ) {
+         // Step 3: Execute a SQL SELECT query
+         String sqlStr = "select password from accounts where username = "
+               + "'" + request.getParameter("username") + "'";   // Single-quote SQL string
+
+         out.println("<h3>Thank you for your query.</h3>");
+         out.println("<p>Your SQL statement is: " + sqlStr + "</p>"); // Echo for debugging
+         ResultSet rset = stmt.executeQuery(sqlStr);  // Send the query to the server
+         rset.next();
+
+         out.println(rset.getString("password"));
+         out.println(request.getParameter("password"));
+         // Step 4: Process the query result set
+         if (rset.getString("password") == request.getParameter("password")){
+            out.println("Log In")
+         }else{
+            out.println("Wrong UserName or Password");
+         }
+      } catch(Exception ex) {
+         out.println("<p>Error: " + ex.getMessage() + "</p>");
+         out.println("<p>Check Tomcat console for details.</p>");
+         ex.printStackTrace();
+      }  // Step 5: Close conn and stmt - Done automatically by try-with-resources (JDK 7)
+ 
+      out.println("</body></html>");
+      out.close();
+   }
+}
